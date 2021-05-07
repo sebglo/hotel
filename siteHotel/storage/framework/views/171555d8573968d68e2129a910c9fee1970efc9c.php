870557b417c616fb1588d091cefc85fa88a66dcf
alx <zz>
2021-05-07 09:45:49 +0200

    removing storage folder from git

--- siteHotel/storage/framework/views/171555d8573968d68e2129a910c9fee1970efc9c.php
+++
@@ -1,5 +0,0 @@
-<?php $__env->startSection('content'); ?>
-<h1>hello ca marche</h1>
-<?php $__env->stopSection(); ?>
-
-<?php echo $__env->make('LayoutPublics.appbis', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH /home/sebastien/projetfinAnnee/hotel/siteHotel/resources/views/LayoutPublics/home.blade.php ENDPATH**/ ?>
